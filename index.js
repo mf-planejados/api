7d787f1d91d22d55a08f6a0648fa823b3d361c47
Marcus-devfs <marcusvini6277@gmail.com>
2023-04-11 19:17:15 -0300

    teste vercel index

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 require('dotenv').config()
 const express = require('express')
-const routes = require('./src/routes')
 const mongoose = require('mongoose')
+const routes = require('./src/routes/index')
 const cors = require('cors')
-
 const app = express()
+
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use(routes)
+app.use('/', routes)
 
 mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URL).then(res => {
    console.log('Connected to DB')
