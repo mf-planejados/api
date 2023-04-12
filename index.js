46689a2170f0e1d611a3d452c6475f134ed6ec71
Marcus-devfs <marcusvini6277@gmail.com>
2023-04-12 00:32:18 -0300

    vercel-teste 6

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use('/', routes)
+app.use(routes)
 
 mongoose.connect(process.env.MONGODB_URL).
     then(() => {
