1c96aebfc386c9ff28fd9ade3493eabc0bd19430
Marcus-devfs <marcusvini6277@gmail.com>
2023-04-12 00:34:54 -0300

    vercel-teste 8

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 const app = express()
-// const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use(routes)
+app.use('/', routes)
 
-mongoose.connect(process.env.MONGODB_URL).
-    then(() => {
-        app.listen(process.env.PORT || '3000')
-        console.log('Conectado ao banco...')
-    })
-    .catch((err) => console.log(err))
+mongoose.connect(process.env.MONGODB_URL).then(res => {
+   console.log('Connected to DB')
+}).catch(err => {
+   console.log('ERRO:', err.errors)
+})
+
+mongoose.Promise = global.Promise
+
+app.listen(port, () => {
+   console.log(`API on port ${port}`)
+})
